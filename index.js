fd3f85abf259ea82d0b6d1249054d41e4e142c9a
huulamnguyen <liamnguyen.swe@gmail.com>
2021-12-15 13:14:20 -0500

    commit tree 4c43536ad2868d1c320013fbef5a9a179741dee1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching = (records, string) => {
+    const res = records.filter(e => e.toLowerCase() === string.toLowerCase());
+    return res
+}
+
+const fuzzyMatch = (records, string) => {
+    const res = records.filter(e => e.toLowerCase().indexOf(string.toLowerCase()) === 0)
+    return res
+}
+
+const matchName = (records, string) => {
+    const res = records.filter(record => record.name === string)
+    return res
+}
